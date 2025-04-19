52bd8afb01b215432c7043ce3561f77fcb2911e6
RachitSrivastava12 <rachitsrivastava76@gmail.com>
2025-04-19 15:39:37 +0530

    adding jwt verification

--- dpin-uptime/apps/api/middleware.ts
+++ dpin-uptime/apps/api/middleware.ts
@@ -1,8 +1,15 @@
 import type { NextFunction, Request, Response } from "express";
-
+import {JWT_PUBLIC_KEY} from "./config";
+import jwt from "jsonwebtoken";
 export function authMiddleware(req: Request, res: Response, next: NextFunction) {
-    const authHeader = req.headers['authorization'];
-
-    req.userId = "8";
+    const token = req.headers['authorization']; 
+    if(!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+    const decoded = jwt.verify(token, JWT_PUBLIC_KEY);
+    if(!decoded || !decoded.sub) {  
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+    req.userId = decoded.sub as string;
     next();
 }
